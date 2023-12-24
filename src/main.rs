c550db55b252165645fd0547b258bbfce4786681
Jack Cogdill <hi@jackcogdill.com>
2023-12-23 19:37:18 -0800

    add list

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,23 @@ impl<'a> fmt::Display for Person<'a> {
     }
 }
 
+#[derive(Debug)]
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let vec = &self.0;
+        write!(f, "[")?;
+        for (count, v) in vec.iter().enumerate() {
+            if count != 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{}", v)?;
+        }
+        write!(f, "]")
+    }
+}
+
 fn main() {
     let name = "Peter";
     let age = 27;
@@ -20,4 +37,8 @@ fn main() {
     println!("display: {}", person);
     println!("debug: {:?}", person);
     println!("pretty debug: {:#?}", person);
+
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
+    println!("{:?}", v);
 }
